42151e15deddbc8e56e544df54818a3a18b75697
chhanda-chakrabarty <chhandachakrabarty7@gmail.com>
2025-04-23 21:06:25 +0600

    spread-rest operator

--- main.js
+++ main.js
@@ -96,17 +96,79 @@
 //   console.log(`${x} x ${i} = ${x * i}`);
 // }
 
-//
+// DOM math table
 
-let input = document.querySelector(".input");
-let btn = document.querySelector(".btn_1");
+// let input = document.querySelector(".input");
+// let btn = document.querySelector(".btn_1");
+// let header = document.querySelector(".header");
 
-// console.log(btn);
+// // let h1 = header.innerHTML;
 
-btn.addEventListener("click", () => {
-  let number = input.value;
+// // console.log(btn);
 
-  for (let i = 1; i <= 10; i++) {
-    console.log(`${number} x ${i} = ${number * i}`);
-  }
-});
+// let button = btn.addEventListener("click", () => {
+//   header.innerHTML = "";
+//   let number = input.value;
+
+//   if (number == "") {
+//     console.log("Nothing here!");
+//   } else {
+//     for (let i = 1; i <= 10; i++) {
+//       header.innerHTML += `${number} x ${i} = ${number * i} <br/>`;
+//     }
+//   }
+// });
+
+// Array Destructuring
+
+// let arr = ["rakib", "sujon", "mamun", "sagor", "Ashjad"];
+
+// let [a, b, c, d, e] = arr;
+
+// let obj = { name: "chhanda", age: 31, address: "Mym", school: "MLHS" };
+
+// let { name, age, address, school } = obj;
+
+// for (let index = 0; index < arr.length; index++) {
+//   const element = arr[index];
+
+//   console.log(element);
+// }
+
+// console.log(school);
+
+// arr.map((item) => {
+//   console.log(item);
+// });
+
+// Spread and Rest Operator
+
+// spread operator
+// let obj = {
+//   name: "chhanda",
+//   age: 23,
+//   nid: 124567,
+//   address: "mym",
+//   contact: 01234514578,
+// };
+
+// let spread = { ...obj, shirt: "black" };
+
+// console.log(spread);
+
+// Rest Operator
+
+// function number(...a) {
+//   console.log(a);
+// }
+
+// number(10, 20, 23, 25, 65, 41, 84, 35, 55);
+
+let arr = ["chhanda", "aytik", "jumma", 32, 45];
+console.log(...arr, "maruf");
+
+let digit = (...a) => {
+  //   console.log(a);
+};
+
+digit("chhanda", 31, 1234567, "MYM", "CyberCrime");
